01e5c6464c5219dbcd41a05da426c3914a2cefc3
Saurabh24197 <s97work@gmail.com>
2018-03-03 14:31:35 +0530

    Initial commit

---
+++ Scripts/PlayerMaster.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerMaster : MonoBehaviour {
+
+    public List<Vector3> movePositions;
+
+    private float waitTime;
+    public float checkTime = 3f;
+    private Tilemap tileMap;
+
+    public void Awake()
+    {
+        movePositions = new List<Vector3>();
+        tileMap = GameObject.FindGameObjectWithTag("MasterTiles").GetComponent<Tilemap>();
+    }
+    public void AddPosition(Vector2 movePos)
+    {
+        
+        float x = movePos.x;
+        float y = movePos.y;
+
+        movePositions.Add(movePos);
+        //go.transform.SetParent(transform);
+    }
+
+    private void Update()
+    {
+        if (Time.time > waitTime)
+        {
+            waitTime=Time.time + checkTime;
+
+            if (movePositions.Count > 5)
+                movePositions.RemoveAt(0);
+        }
+    }
+}
